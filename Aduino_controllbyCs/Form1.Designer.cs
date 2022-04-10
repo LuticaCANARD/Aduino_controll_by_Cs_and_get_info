8e246aaccf29ae8291730b1c77712948d6011c44
LuticaCANARD <presan100@gmail.com>
2022-04-10 23:53:17 +0900

    초기 커밋.

---
+++ Aduino_controllbyCs/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Aduino_controllbyCs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sentBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.writeSignal = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.getSignal = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            resources.ApplyResources(this.comboBox2, "comboBox2");
+            this.comboBox2.Name = "comboBox2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sentBtn
+            // 
+            resources.ApplyResources(this.sentBtn, "sentBtn");
+            this.sentBtn.Name = "sentBtn";
+            this.sentBtn.UseVisualStyleBackColor = true;
+            this.sentBtn.Click += new System.EventHandler(this.sentBtn_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // writeSignal
+            // 
+            this.writeSignal.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            resources.ApplyResources(this.writeSignal, "writeSignal");
+            this.writeSignal.Name = "writeSignal";
+            this.writeSignal.UseWaitCursor = true;
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // getSignal
+            // 
+            this.getSignal.Cursor = System.Windows.Forms.Cursors.Default;
+            resources.ApplyResources(this.getSignal, "getSignal");
+            this.getSignal.Name = "getSignal";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.getSignal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sentBtn);
+            this.Controls.Add(this.writeSignal);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button sentBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox writeSignal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox getSignal;
+    }
+}
+
---
+++ Aduino_controllbyCs/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Aduino_controllbyCs
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort arduinoSerial;
+        private string[] frequeny = { "9600","19200","4800" };
+        public Form1()
+        {
+            InitializeComponent();
+            arduinoSerial = new SerialPort();
+            string[] sp = SerialPort.GetPortNames();
+            comboBox1.DataSource = sp;
+            comboBox2.DataSource = frequeny;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Activated(object sender, System.EventArgs e)
+        {
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (arduinoSerial.IsOpen)
+            {
+                arduinoSerial.Close();
+            }
+            if(!arduinoSerial.IsOpen)
+            {
+                arduinoSerial.PortName = comboBox1.Text;
+                arduinoSerial.BaudRate =int.Parse(comboBox2.Text);
+                arduinoSerial.DataBits = 8;
+                arduinoSerial.Parity = Parity.None;
+                arduinoSerial.StopBits = StopBits.One;
+                arduinoSerial.DataReceived += new SerialDataReceivedEventHandler(arduinoSerial_DataReceived);
+
+                try
+                {
+                    arduinoSerial.Open();
+                   
+                }
+                catch (Exception ex)
+                {
+                    getSignal.Text = ex.Message;
+                }
+            }
+            else
+            {
+                getSignal.Text = "지정된" + comboBox1.Text + "는 이미 열려있음";
+            }
+
+        }
+        private byte[] strToByte(string str)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(str);
+            return bytes; 
+        }
+        private string byteToStr(byte[] bt)
+        {
+            string str = Encoding.UTF8.GetString(bt);
+            return str;
+        }
+
+        private void sentBtn_Click(object sender, EventArgs e)
+        {
+            
+            if (arduinoSerial.IsOpen == false)
+            {
+                return;
+            }
+            /* string mes = writeSignal.Text + "\n";
+             byte[] datas = strToByte(mes); //'1' 전송 뒤에 개행문자 \n을 필수로 붙혀야 함
+             arduinoSerial.Write(datas, 0, datas.Length);
+             label5.Text = mes;*/
+            arduinoSerial.WriteLine(writeSignal.Text + "\n");
+        }
+        private void get_byte()
+        {
+            getSignal.Text = arduinoSerial.ReadLine();
+        }
+
+        private void getSignal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void arduinoSerial_DataReceived(object o, EventArgs e)
+        {
+            if (arduinoSerial.IsOpen == false) { return; };
+
+                string resive_datap = arduinoSerial.ReadLine();
+                if (this.getSignal.InvokeRequired == true)
+                {
+                    this.getSignal.Invoke((MethodInvoker)delegate
+                     {
+                         getSignal.Text += string.Format("{0} ", resive_datap);
+                     });
+                }
+                else
+                {
+                    getSignal.Text += string.Format("{0} ", resive_datap);
+                }
+           
+        }
+    }
+}
